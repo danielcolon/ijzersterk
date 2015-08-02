23a1b6d9344fdd4ebdb22e380520982ae4ceec83
dcolon <abdicoveritas@gmail.com>
2015-08-02 15:53:49 +0200

    Made user endpoint more robust and prettier with switches and stuff

--- api/classes/ijzersterkAPI.php
+++ api/classes/ijzersterkAPI.php
@@ -76,134 +76,157 @@
 			require_once("$dirname/../lib/cookies.php");
 			require_once("$dirname/../lib/sessions.php");
 	
-			if($this->verb == "" && $this->method == 'GET')
-			{
-				//Return a list of usernames
-				// Only admins can see all users
-				if($this->currentUser->getIsAdmin())
-				{
-					$AUsers = new users;
-					if($AUsers->fill())
-					{
-						$this->responseCode = 200;
-						return json_encode(array("status" => "200 OK", "details" => "List of users returned", "result" => $AUsers->getAsArray()));
+			switch($this->verb){
+				// Empty verb means user requested a list of usernames
+				case "":
+					switch($this->method){
+						case 'GET':
+							// Return a list of usernames
+							// Only admins can see all users
+							if($this->currentUser->getIsAdmin())
+							{
+								$AUsers = new users;
+								if($AUsers->fill())
+								{
+									$this->responseCode = 200;
+									return json_encode(array("status" => "200 OK", "details" => "List of users returned", "result" => $AUsers->getAsArray()));
+								}
+								else
+								{
+									$this->responseCode = 500;
+									return json_encode(array("status" => "500 Internal Server Error","details" => "Couldn't load users"));
+								}
+							}
+							$this->responseCode = 403;
+							return json_encode(array("status" => "403 Forbidden","details" => "Admin rights required"));
+						break;
+						default:
+							$this->responseCode = 405;
+							return json_encode(array("status" => "405 Method Not Allowed","details" => "Method Not Allowed"));
+						break;
 					}
-					else
-					{
-						$this->responseCode = 500;
-						return json_encode(array("status" => "500 Internal Server Error","details" => "Couldn't load users"));
-					}
-				}
-				$this->responseCode = 403;
-				return json_encode(array("status" => "403 Forbidden","details" => "Admin rights required"));
-			}
-			else if($this->method == 'GET')
-			{
-				// Only let admins or the user themselves see the user
-				if($this->verb == $this->currentUser->getUsername() || $this->currentUser->getIsAdmin())
-				{
-					// See if t's the logged in user, if so, return it
-					if($this->verb == $this->currentUser->getUsername())
-					{
-						$this->responseCode = 200;
-						return json_encode(array("status" => "200 OK", "details" => "Current user returned", "result" => $this->currentUser->getAsAssociativeArray()));
-					}
-
-					// Try to fill for the given username
-					$Auser = new user;
-					$Auser->setUsername($this->verb);
-					if($Auser->fill())
-					{
-						return json_encode(array("status" => "200 OK", "details" => "User returned", "result" => $Auser->getAsAssociativeArray()));
-					}
-					else
-					{
-						$this->responseCode = 404;
-						return json_encode(array("status" => "404 Not Found","details" => "Couldn't find user " . $this->verb));
-					}
-				}
-				else
-				{
-					$this->responseCode = 403;
-					return json_encode(array("status" => "403 Forbidden","details" => "Admin rights required"));
-				}
-			}
-			else if($this->verb == "verify" && $this->method == 'POST')
-			{
-				// This is a public function to check username and password
-				// Always respond 200
-				$this->responseCode = 200;
-
-				$this->currentUser = new user;
-				if(!$this->currentUser->login($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']))
-				{
-					$result = array("isOk" => FALSE);
-				}
-				else
-				{
-					$result = array("isOk" => TRUE);
-				}
-				return json_encode(array("status" => "200 OK", "details" => "User verified", "result" => $result));
-			}
-			else if($this->verb != "" && $this->method == 'PUT')
-			{
-				// We're either going to make a new user or update one
-				// Either way you can only change your own user unless you're an admin
-				if($this->verb == $this->currentUser->getUsername() || $this->currentUser->getIsAdmin())
-				{
-					// Create a user
-					$AUser = new user;
-					$AUser->setUsername($this->verb);
-
-					// Get the data being put
-					$PUTArray = json_decode($this->file, true);
-
-					// Check if it worked
-					if(is_null($PUTArray))
-					{
-						$this->responseCode = 400;
-						return json_encode(array("status" => "400 Bad Request","details" => "Couldn't decode json input data"));
-					}
-
-					if(!$AUser->fill() && !isset($PUTArray["password"]))
-					{
-						$this->responseCode = 400;
-						//If the user doesn't exist yet we need to make sure there's a new password in the data
-						return json_encode(array("status" => "400 Bad Request","details" => "No password provided for new user"));
-					}
-
-					// Remember if we're a new user
-					$isNew = !$AUser->getInDatabase();
-
-					// If we get a password we'll have to hash it
-					if(isset($PUTArray["password"]))
-					{
-						$AUser->setPassword(password_hash($PUTArray["password"], PASSWORD_DEFAULT));
-					}
-
-					// Now get the other stuff
-					$AUser->setUsername($this->verb);
-					if(isset($PUTArray["isAdmin"]) && $this->currentUser->getIsAdmin()) $AUser->setIsAdmin($PUTArray["isAdmin"]); // Only let admins set isadmin
-					if(isset($PUTArray["firstName"])) $AUser->setFirstName($PUTArray["firstName"]);
-					if(isset($PUTArray["lastName"])) $AUser->setLastName($PUTArray["lastName"]);
-					if(isset($PUTArray["email"])) $AUser->setEmail($PUTArray["email"]);
-
-					// The user object will handle whether we'll add a user or update a user depending on if
-					// it was found in the database or not
-					if($AUser->addToDatabase())
-					{
-						if($isNew) 
-						{
-							$this->responseCode = 201;
-							return json_encode(array("status" => "201 Created", "details" => "New user succesfully created","result" => $AUser->getAsAssociativeArray()));
-						}
-						else
-						{
+				break;
+				// user/verify is a public function to check username and password
+				case "verify":
+					switch($this->method){
+						case 'POST':
+							// Always respond 200
 							$this->responseCode = 200;
-							return json_encode(array("status" => "200 Ok", "details" => "User succesfully updated","result" => $AUser->getAsAssociativeArray()));
-						}
+
+							$this->currentUser = new user;
+							if(!$this->currentUser->login($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']))
+							{
+								$result = array("isOk" => FALSE);
+							}
+							else
+							{
+								$result = array("isOk" => TRUE);
+							}
+							return json_encode(array("status" => "200 OK", "details" => "User verified", "result" => $result));
+						break;
+						default:
+							$this->responseCode = 405;
+							return json_encode(array("status" => "405 Method Not Allowed","details" => "Method Not Allowed"));
+						break;
 					}
-				}
+				break;
+				// Any verb that's not empty or verify means we'll have to look for a user with that name
+				default:
+					switch($this->method){
+						case 'GET':
+							// Only let admins or the user themselves see the user
+							if($this->verb == $this->currentUser->getUsername() || $this->currentUser->getIsAdmin())
+							{
+								// See if it's the logged in user, if so, return it
+								if($this->verb == $this->currentUser->getUsername())
+								{
+									$this->responseCode = 200;
+									return json_encode(array("status" => "200 OK", "details" => "Current user returned", "result" => $this->currentUser->getAsAssociativeArray()));
+								}
+								// Try to fill for the given username
+								$Auser = new user;
+								$Auser->setUsername($this->verb);
+								if($Auser->fill())
+								{
+									return json_encode(array("status" => "200 OK", "details" => "User returned", "result" => $Auser->getAsAssociativeArray()));
+								}
+								else
+								{
+									$this->responseCode = 404;
+									return json_encode(array("status" => "404 Not Found","details" => "Couldn't find user " . $this->verb));
+								}
+							}
+							else
+							{
+								$this->responseCode = 403;
+								return json_encode(array("status" => "403 Forbidden","details" => "Admin rights required"));
+							}
+						break;
+						case 'PUT':
+							// We're either going to make a new user or update one
+							// Either way you can only change your own user unless you're an admin
+							if($this->verb == $this->currentUser->getUsername() || $this->currentUser->getIsAdmin())
+							{
+								// Create a user
+								$AUser = new user;
+								$AUser->setUsername($this->verb);
+
+								// Get the data being put
+								$PUTArray = json_decode($this->file, true);
+
+								// Check if it worked
+								if(is_null($PUTArray))
+								{
+									$this->responseCode = 400;
+									return json_encode(array("status" => "400 Bad Request","details" => "Couldn't decode json input data"));
+								}
+
+								if(!$AUser->fill() && !isset($PUTArray["password"]))
+								{
+									$this->responseCode = 400;
+									//If the user doesn't exist yet we need to make sure there's a new password in the data
+									return json_encode(array("status" => "400 Bad Request","details" => "No password provided for new user"));
+								}
+
+								// Remember if we're a new user
+								$isNew = !$AUser->getInDatabase();
+
+								// If we get a password we'll have to hash it
+								if(isset($PUTArray["password"]))
+								{
+									$AUser->setPassword(password_hash($PUTArray["password"], PASSWORD_DEFAULT));
+								}
+
+								// Now get the other stuff
+								$AUser->setUsername($this->verb);
+								if(isset($PUTArray["isAdmin"]) && $this->currentUser->getIsAdmin()) $AUser->setIsAdmin($PUTArray["isAdmin"]); // Only let admins set isadmin
+								if(isset($PUTArray["firstName"])) $AUser->setFirstName($PUTArray["firstName"]);
+								if(isset($PUTArray["lastName"])) $AUser->setLastName($PUTArray["lastName"]);
+								if(isset($PUTArray["email"])) $AUser->setEmail($PUTArray["email"]);
+
+								// The user object will handle whether we'll add a user or update a user depending on if
+								// it was found in the database or not
+								if($AUser->addToDatabase())
+								{
+									if($isNew) 
+									{
+										$this->responseCode = 201;
+										return json_encode(array("status" => "201 Created", "details" => "New user succesfully created","result" => $AUser->getAsAssociativeArray()));
+									}
+									else
+									{
+										$this->responseCode = 200;
+										return json_encode(array("status" => "200 Ok", "details" => "User succesfully updated","result" => $AUser->getAsAssociativeArray()));
+									}
+								}
+							}
+						break;
+						default:
+							$this->responseCode = 405;
+							return json_encode(array("status" => "405 Method Not Allowed","details" => "Method Not Allowed"));
+						break;
+					}
+				break;
 			}
 
 			// Apparantly somethign went wrong if we still haven't returned
