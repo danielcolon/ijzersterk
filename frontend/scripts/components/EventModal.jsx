e3a45e7c249275601871d70fd3dd139f0b66ecc0
Pouja Nikray <poujanik@gmail.com>
2015-08-31 19:46:36 +0200

    Cleaned up the EventModal. All the buttons (save, edit, cancel, close etc) are now properly shown

--- frontend/scripts/components/EventModal.jsx
+++ frontend/scripts/components/EventModal.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button, ButtonGroup} from 'react-bootstrap';
 import Agenda from '../models/Agenda.js';
 import moment from 'moment';
 import EL from '../EventListener.js';
 
 export default React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
+    refresh(eventId){
+        eventId = (eventId !== undefined && eventId !== null) ? eventId : this.props.eventId;
+        this.setState({
+            model: Agenda.getEvent(eventId)
+        });
+    },
     getInitialState(){
-        return Agenda.getEvent(this.props.eventId);
+        return {
+            view: 'edit',
+            model: Agenda.getEvent(this.props.eventId)
+        };
+    },
+    componentWillMount(){
+        EL.on('event.changed', () => this.refresh());
     },
     componentWillReceiveProps(nextProps){
-        this.setState(Agenda.getEvent(nextProps.eventId));
+        this.setState({
+            view: (nextProps.eventId === 'new') ? 'edit' : 'view'
+        });
+        this.refresh(nextProps.eventId);
     },
-    leave(){
-        Agenda.leave(this.state);
-        this.setState(Agenda.getEvent(this.props.eventId));
+    viewMode(){
+        this.setState({
+            view: 'view'
+        });
     },
-    attend(){
-        Agenda.attend(this.state);
-        this.setState(Agenda.getEvent(this.props.eventId));
+    editMode(){
+        this.setState({
+            view: 'edit'
+        });
     },
-    render(){
-        var event = this.state;
-        var startDate = moment(event.start);
-        var endDate = moment(event.end);
-        return <Modal {...this.props}>
-                <Modal.Header closeButton>
-                    <Modal.Title>{event.title} - {startDate.format('D MMM HH:mm')} <span> till </span>
-                    {(endDate.isSame(startDate, 'day')) ?
-                        endDate.format('HH:mm') : endDate.format('D MMM HH:mm')} </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
+    renderView(){
+        var event = this.state.model;
+        return <Modal.Body>
                     {event.description}<br/>
                     Attendees:
                     <ul>
@@ -39,14 +49,52 @@ export default React.createClass({
                             }) : <i>None, be the first to register!</i>
                         }
                     </ul>
-                </Modal.Body>
+                </Modal.Body>;
+    },
+    renderEdit(){
+        return <Modal.Body></Modal.Body>;
+    },
+    save(){
+
+    },
+    renderEditShow(){
+        var save = <Button key={1} onClick={this.save} bsStyle="success">Save</Button>;
+        var cancel = <Button key={2} onClick={this.viewMode}>Cancel</Button>;
+        var edit = <Button key={3} onClick={this.editMode}>Edit</Button>;
+        var attend = <Button key={4} onClick={()=> Agenda.attend(this.state.model)} bsStyle="success">Attend</Button>;
+        var leave = <Button key={5} onClick={()=> Agenda.leave(this.state.model)} bsStyle="danger">Leave</Button>;
+        var close = <Button key={6} onClick={this.props.onHide}>Close</Button>;
+        var buttons = [];
+
+        if (this.state.view === 'edit') {
+            buttons.push(save, cancel);
+        } else {
+            if (Agenda.isAttending(this.state.model)){
+                buttons.push(leave);
+            } else {
+                buttons.push(attend);
+            }
+            if (Agenda.canEdit(this.state.model)){
+                buttons.push(edit);
+            }
+        }
+        buttons.push(close);
+
+        return <ButtonGroup>{buttons}</ButtonGroup>;
+    },
+    render(){
+        var event = this.state.model;
+        var startDate = moment(event.start);
+        var endDate = moment(event.end);
+        return <Modal {...this.props}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{event.title} - {startDate.format('D MMM HH:mm')} <span> till </span>
+                    {(endDate.isSame(startDate, 'day')) ?
+                        endDate.format('HH:mm') : endDate.format('D MMM HH:mm')} </Modal.Title>
+                </Modal.Header>
+                {(this.state.view === 'edit') ? this.renderEdit() : this.renderView()}
                 <Modal.Footer>
-                    {(Agenda.isAttending(event)) ?
-                        <Button onClick={this.leave} bsStyle="danger">Leave</Button> :
-                        <Button onClick={this.attend} bsStyle="success">Attend</Button>}
-                    <Button onClick={EL.partialEmit('editEvent', event.id)} style={{
-                        display: (Agenda.canEdit(event)) ? 'initial' : 'none'}}>Edit</Button>
-                    <Button onClick={this.props.onHide}>Close</Button>
+                    {this.renderEditShow()}
                 </Modal.Footer>
             </Modal>;
     }
