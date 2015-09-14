c91be15029efa3e17751955e4627d2f664b6ebcd
Pouja Nikray <poujanik@gmail.com>
2015-09-14 12:16:11 +0200

    Added joey meet blog and auke clinic

--- frontend/scripts/views/Blogs.jsx
+++ frontend/scripts/views/Blogs.jsx
@@ -5,6 +5,52 @@ default React.createClass({
     render() {
         return (
             <div className="col-md-8 col-md-offset-2 col-xs-12">
+                <div id="first-meet-joey" className="article">
+                    <h1>My first powerlifting meet</h1>
+                    <div className="date">September 14, 2015 | <span className="name">Joey</span> | 5 min read</div>
+                    <p><b>I have been lifting since I was 16 years old for general fitness, bodybuilding, injury recovery and for the past two years powerlifting. Yet I had never participated in any competition or meet up until last Sunday. I was lacking motivation and first-hand competition experience which all of other members of the board did have. So I decided to give it a shot… </b></p><br/>
+                    <div className="col-xs-12">
+                        <div className="col-xs-11 thumbnail">
+                            <img src="img/meet-joey-3.jpg"/>
+                            <div className="caption">
+                                195Kg squat
+                            </div>
+                        </div>
+                    </div>
+                    <h4>How did I prepare for the big day? </h4>
+                    <p>In terms of training I barely prepared. The reason is that I did not have many choices in competitions to participate in. The SBD Cup was de last bond-organized meet this year in which you could qualify yourself for the Dutch nationals in December. So it came down to a month of time to prepare, but I would also be on holiday for two weeks. I tried to determine my opening weights before I left and during my holiday I managed to slip in one workout. After I came back I had a full week left in which I had four workouts focusing mainly on form and I cut some water to keep my weight in check. Then I had two days of rest and I was just hoping for the best.</p>
+                    <h4>Lightweight!</h4>
+                    <p>I was in the first session of the day with the weigh-in starting at 7:30. It surprised me when I ended up weighing only 80.8kg (-83kg class). I applied water cut in the week leading up to the competition and part of that is no drinking or eating starting 12 hours before the weigh-in. After the weigh-in you got approximately two hours to rehydrate and eat, during this time you should focus on carbs, minerals and water. With Pouja as my coach we had tons of food and drinks with us so then the waiting game began.</p>
+                    <h4>Warming up and opening with squats</h4>
+                    <p>30 Minutes before the first lift we started warming up and to our horror we found out that we had to warm up outside in the cold! So, always bring warm clothes and remember that circumstances will often be far from ideal. I changed my opener on the squat last minute with 5kg to 175kg as I was feeling quite good. My feeling was right as I squatted 3/3 with respectively 175kg, 185kg, 195kg(+5 PR!). It boosted my confidence and I was ready for the next lift.</p>
+                    <h4>Signals and ego lifting</h4>
+                    <p>Bench press has always been one of my weakest lifts and I was not too confident in my form. My opener of 110kg was no lift because I did not wait for the referee’s signal. The weight itself was not a problem so I did 115kg on my second attempt which went fine. From then on it went downhill. I increased my planned third attempt by 2.5kg to 122.5kg but I lost focus and form during the attempt and failed. I am not sure if it was a case of ego lifting, fatigue or just bad focus. If you want to learn more about ego lifting you should check out the previous blog post. There was no time to waste and I had to put my thoughts on the last and my best lift, the deadlift. </p>
+                    <h4>Playing safe or taking risks</h4>
+                    <p>First off, I love deadlifting. I knew I could get back into the competition with deadlifts. I opened 220kg with good form and speed. 235kg for the second attempt with good form and slightly less speed. For the final lift it was a toss-up between 245kg (gym PR) or 250kg (tried once in gym and failed lift-off). I took the risk and went with the 250kg and failed on grip, which really surprised me. Looking back I am not sure if 245kg would have made a big difference. One of the things I am still struggling with is consistency in the pre-lift rituals. It took me a lot longer to actually start the lift with the third attempt compared to the other attempts. As a result I lost confidence. I tend to have this problem when I am lifting weights I am not comfortable with and often. Ideally, you want to have the same pre-lift ritual with every rep of every set, even your warm-up sets! </p>
+                    <h4>Qualified and now what?</h4>
+                    <p>My total of 545kg at 80.8kg bodyweight placed me second of 11 participants in the men’s open classic -83kg. All lifters with Wilks above 340 points were qualified for the nationals and so was I (369.8 Wilks). In the end I have no regrets getting into the world of powerlifting competitions. It was inspirational, I got to know lots of people with the same interest and it’s a great way to stay motivated and (officially) log your progress. </p>
+                    <p>Now I have three months to properly prepare for the nationals and I am going to follow a Sheiko program (#37v2, #31, #32v2). This is a Russian powerlifting program with the unique emphasis on volume rather than high weights. It seems like a fitting approach for me as I have to work on my form (esp. bench) and the way of training appeals to me. If you are interested in following the program yourself you can check out his website <a href="http://sheiko-program.ru/">http://sheiko-program.ru/</a>. Just interested in my progress? Follow my training log on <a href="http://weightxreps.net/journal/Quadzilla">http://weightxreps.net/journal/Quadzilla</a> </p>
+                    <p>All lifts can be find <a href="https://www.youtube.com/playlist?list=PL1TKZz6hBlThOT1zZ1-3Vlrh0YQD_GKfo">here</a></p>
+                    <p>The award ceremony</p>
+                    <div className="row">
+                        <div className="col-md-6 col-xs-12">
+                            <div className="thumbnail">
+                                <img src="img/meet-joey-1.jpg"/>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-xs-12">
+                            <div className="thumbnail">
+                                <img src="img/meet-joey-2.jpg"/>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div id="clinic-2015" className="article">
+                    <h1>IJzersterk Clinic</h1>
+                    <div className="date">September 7, 2015 | <span className="name">Auke</span></div>
+                    <p>It was a hot summer afternoon with big rainfall and thunder, but there was one thing that overcame the shivering that Mother Nature directed to us, and that was the first IJZERSTERK clinic.. A good amount of enthusiastic and curious new students came to see what we are all about. After the opening speech by our president, Peter Evers, we started off with instructions on the big three lifts. The board helped everybody out with the techniques of the squat, bench and deadlift and gave some demo’s. The atmosphere was very relaxing yet uplifting and even some PR’s were broken, this shows the potential of lifting together with IJzersterk. We all had a splendid time with all of the participants and hopefully we will see you guys in the gym and joining our iron strong squad!</p>
+                </div>
                 <div id="ego-lifting" className="article">
                     <h1>Ego lifting</h1>
                     <div className="date">August 26, 2015 | <span className="name">Pouja</span> | 3 min read</div>
--- frontend/scripts/views/Sidebar.jsx
+++ frontend/scripts/views/Sidebar.jsx
@@ -11,14 +11,17 @@ default React.createClass({
             <div className="block">
                 <h4>News</h4>
                 <ul>
+                    <li><Link to="/blogs#clinic-2015">IJzersterk Clinic</Link></li>
                     <li><Link to="/blogs#website-live">New website is finally live</Link></li>
                     <li><Link to="/blogs#newsletter2">Newsletter #2</Link></li>
-                    <li><Link to="/blogs#sterkstestudent2015">Sterkste Student(e) van Delft</Link></li>
                 </ul>
             </div>
             <div className="block">
                 <h4>Blogs</h4>
                 <ul>
+                    <li>
+                        <Link to="/blogs#first-meet-joey">My first powerlifting meet - by Joey</Link>
+                    </li>
                     <li>
                         <Link to="/blogs#ego-lifting">
                             Ego lifting - The good, the bad and the ugly
